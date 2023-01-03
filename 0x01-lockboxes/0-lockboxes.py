16940feb9590fad724a52fb2d592217edaead3b1
Natiman58 <natiman58@gmail.com>
2023-01-04 00:15:29 +0300

    lock box

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+def canUnlockAll(boxes):
+    keys = [0]
+    """ loop through boxes array and
+        find the index of each element(inner boxes)
+    """
+    for index, item in enumerate(boxes):
+        if not item:
+            continue
+        for key in item:
+            """ loop through each key in item(inner box)
+                and append the key to keys array
+            """
+            if key < len(boxes) and key not in keys and key != index:
+                keys.append(key)
+    """ if the number of keys is equal to number of inner boxes
+        return True; else False
+    """
+    if len(keys) == len(boxes):
+        return True
+    return False
