add544ae9e6abc6bd7a7c252921e2248cb75d043
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-28 21:39:45 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    calculate the perimeter of an island
+"""
+from typing import List
+
+
+def island_perimeter(grid: List[List]) -> int:
+    """
+        grid: collection of islands
+        return: their perimeter
+    """
+    new_arr = []
+    for i in range(len(grid)):
+        for j in range(i + 1):
+            if grid[i][j] == 1:
+                new_arr.append(grid[i][j])
+    return (len(new_arr) * 4) - (2 * len(new_arr) - 2)
