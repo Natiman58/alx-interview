576bc2816db53d37688d277854d7b5f3e0f9d50a
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-01 12:43:45 +0300

    scan the surrounding

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,9 +9,28 @@ def island_perimeter(grid):
         grid: collection of islands
         return: their perimeter
     """
-    new_arr = []
+    perimeter = 0
+    # i -> row , j -> col
     for i in range(len(grid)):
         for j in range(i + 1):
+            # if its a single island increase perimeter by 4
             if grid[i][j] == 1:
-                new_arr.append(grid[i][j])
-    return (len(new_arr) * 4) - (2 * len(new_arr) - 2)
+                perimeter += 4
+                # if the is an island infront of the current island
+                if grid[i - 1][j] == 0 and grid[i + 1][j] == 1 and grid[i][j + 1] == 0 and grid[i][j - 1] == 0:
+                    perimeter -= 2
+                # if there is's between islands(front and back)
+                if grid[i - 1][j] == 1 and grid[i + 1][j] == 1 and grid[i][j + 1] == 0 and grid[i][j - 1] == 0:
+                    perimeter += 2
+                # if there is an island in the back and on the right
+                if grid[i - 1][j] == 1 and grid[i + 1][j] == 0 and grid[i][ j + 1] == 1 and grid[i][j - 1] == 0:
+                    perimeter -= 4
+                # if there are islands on the back and left
+                if grid[i - 1][j] == 1 and grid[i + 1][j] == 0 and grid[i][ j + 1] == 0 and grid[i][j - 1] == 1:
+                    perimeter -= 4
+                # if in betweeen island (left and right)
+                if grid[i - 1][j] == 0 and grid[i + 1][j] == 0 and grid[i][ j + 1] == 1 and grid[i][j - 1] == 1:
+                    perimeter -= 4
+    return perimeter
+
+                    
