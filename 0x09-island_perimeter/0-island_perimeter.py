ae1a1cfb36be3d5e3793ad449cc717b40022bfc5
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-01 17:03:45 +0300

    pycode

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -23,25 +23,25 @@ def island_perimeter(grid):
 
                 # if there is an island infront of the current island
                 if back == 0 and front == 1 and right == 0 and left == 0:
-                    #print(perimeter)
+                    # print(perimeter)
                     perimeter += 2
-                    #print(perimeter)
+                    # print(perimeter)
                 # if is's between islands(front and back)
                 elif back == 1 and front == 1 and right == 0 and left == 0:
                     perimeter -= 2
-                    #print(perimeter)
+                    # print(perimeter)
                 # if there is an island in the back and on the right
                 elif back == 1 and front == 0 and right == 1 and left == 0:
-                    #print(perimeter)
+                    # print(perimeter)
                     perimeter -= 4
-                    #print(perimeter)
+                    # print(perimeter)
 
                 # if there are islands on the back and left
                 elif back == 1 and front == 0 and right == 0 and left == 1:
                     perimeter += 0
-                    #print(perimeter)
+                    # print(perimeter)
                 # if in betweeen island (left and right)
                 elif back == 0 and front == 0 and right == 1 and left == 1:
                     perimeter -= 0
-                    #print(perimeter)
+                    # print(perimeter)
     return perimeter - 4
