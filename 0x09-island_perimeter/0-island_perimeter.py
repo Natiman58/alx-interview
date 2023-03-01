0ffeb091cf3318bf2d7640bef09a92eebe443aa1
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-01 12:50:31 +0300

    pycode

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,21 +16,24 @@ def island_perimeter(grid):
             # if its a single island increase perimeter by 4
             if grid[i][j] == 1:
                 perimeter += 4
+                back = grid[i - 1][j]
+                front = grid[i + 1][j]
+                left = grid[i][j - 1]
+                right = grid[i][j + 1]
+
                 # if the is an island infront of the current island
-                if grid[i - 1][j] == 0 and grid[i + 1][j] == 1 and grid[i][j + 1] == 0 and grid[i][j - 1] == 0:
+                if back == 0 and front == 1 and right == 0 and left == 0:
                     perimeter -= 2
                 # if there is's between islands(front and back)
-                if grid[i - 1][j] == 1 and grid[i + 1][j] == 1 and grid[i][j + 1] == 0 and grid[i][j - 1] == 0:
+                if back == 1 and front == 1 and right == 0 and left == 0:
                     perimeter += 2
                 # if there is an island in the back and on the right
-                if grid[i - 1][j] == 1 and grid[i + 1][j] == 0 and grid[i][ j + 1] == 1 and grid[i][j - 1] == 0:
+                if back == 1 and front == 0 and right == 1 and left == 0:
                     perimeter -= 4
                 # if there are islands on the back and left
-                if grid[i - 1][j] == 1 and grid[i + 1][j] == 0 and grid[i][ j + 1] == 0 and grid[i][j - 1] == 1:
+                if back == 1 and front == 0 and right == 0 and left == 1:
                     perimeter -= 4
                 # if in betweeen island (left and right)
-                if grid[i - 1][j] == 0 and grid[i + 1][j] == 0 and grid[i][ j + 1] == 1 and grid[i][j - 1] == 1:
+                if back == 0 and front == 0 and right == 1 and left == 1:
                     perimeter -= 4
     return perimeter
-
-                    
