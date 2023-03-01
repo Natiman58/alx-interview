727d0055cc4f50c5a14f88491ae649e9285c05c4
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-01 17:55:10 +0300

    fix the length

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -20,7 +20,7 @@ def island_perimeter(grid):
     perimeter = 0
     # i -> row , j -> col
     for i in range(len(grid)):
-        for j in range(i + 1):
+        for j in range(len(grid[0])):
             # if its a single island increase perimeter by 4
             if grid[i][j] == 1:
                 perimeter += 4
