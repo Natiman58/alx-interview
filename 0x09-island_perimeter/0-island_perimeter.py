380eb82e1138e9e59829391c11619c50b17acdeb
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-03 00:56:00 +0300

    first cell

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -20,18 +20,18 @@ def island_perimeter(grid):
     perimeter = 0
     # i -> row , j -> col
     for i in range(len(grid)):
-        for j in range(1, len(grid[0])):
+        for j in range(len(grid[0])):
             # if its a single island increase perimeter by 4
             if grid[i][j] == 1:
                 perimeter += 4
-                # below = grid[i + 1][j] and right = grid[i][j + 1]
+                #below = grid[i + 1][j], right = grid[i][j + 1]
                 left = grid[i][j - 1]
                 top = grid[i - 1][j]
 
                 # check if there is an island to the left of the current island
-                if left == 1:
+                if j > 0 and left == 1:
                     perimeter -= 2
                 # the check if there is an island on top of the current island
-                if top == 1:
+                if j > 0 and top == 1:
                     perimeter -= 2
     return perimeter
